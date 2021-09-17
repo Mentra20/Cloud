3bf7819580d634a49f45f967f86d6a4b3a258630
Mentra20 <yann.martindes@orange.fr>
2021-09-17 17:13:15 +0200

    First version of application

---
+++ cloud/src/main/java/org/si5/cloud/HelloController.java
@@ -0,0 +1,14 @@
+package org.si5.cloud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World !";
+    }
+
+}
