9024bf0cf4b116cdfb32837688a07d86ce423d47
Mentra20 <yann.martindes@orange.fr>
2021-09-25 17:31:45 +0200

    Last step for heroku

--- src/main/java/org/si5/cloud/Application.java
+++ src/main/java/org/si5/cloud/Application.java
@@ -1,13 +1,18 @@
 package org.si5.cloud;
 
-import org.si5.cloud.database.CompteurRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app =new SpringApplication(Application.class);
+
+        String port = System.getenv().getOrDefault("PORT", "80");
+        app.setDefaultProperties(Collections.singletonMap("server.port", port));
+
+        app.run(args);
     }
 }
